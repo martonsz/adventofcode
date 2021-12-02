256641c8ef6978fc518476c24af40edfdb91e994
Marton Szucs <marton@tempusinfo.com>
2021-12-02 12:29:51 +0100

    2021: day 2 part B solved

--- 2021/02/aoc02.go
+++ 2021/02/aoc02.go
@@ -54,14 +54,40 @@ func partA() {
 
 		horizontal += a.horizontal
 		depth += a.depth
-		fmt.Printf("%v %v %v, %v\n", a, horizontal, depth, horizontal*depth)
+		//fmt.Printf("%v %v %v, %v\n", a, horizontal, depth, horizontal*depth)
 	}
+
+	fmt.Printf("Part B: %v\n", horizontal*depth)
 
 	check(scanner.Err())
 }
 
 func partB() {
+	// f, _ := os.Open("02/input-example.txt")
+	f, _ := os.Open("02/input.txt")
+	defer f.Close()
 
+	scanner := bufio.NewScanner(f)
+
+	horizontal := 0
+	depth := 0
+	aim := 0
+	for scanner.Scan() {
+		a := parseLine(scanner.Text())
+
+		if a.horizontal > 0 {
+			// It increases your horizontal position by X units.
+			// It increases your depth by your aim multiplied by X.
+			horizontal += a.horizontal
+			depth += a.horizontal * aim
+		} else {
+			aim += a.depth
+		}
+		//fmt.Printf("a:%v h:%v d:%v, a:%v, m:%v\n", a, horizontal, depth, aim, horizontal*depth)
+	}
+	fmt.Printf("Part B: %v\n", horizontal*depth)
+
+	check(scanner.Err())
 }
 
 func main() {
