dfe95b56d3959ea3ad424ef665529f584979ece0
Marton Szucs <marton.szucs@phenixidentity.com>
2022-12-02 08:53:00 +0100

    2022: day02 part B  solved

--- day02/app.go
+++ day02/app.go
@@ -25,6 +25,14 @@ func printCWD() {
 
 func part1(lines []string) {
 
+	moveMap := make(map[byte]int)
+	moveMap['A'] = 0
+	moveMap['B'] = 1
+	moveMap['C'] = 2
+	moveMap['X'] = 0
+	moveMap['Y'] = 1
+	moveMap['Z'] = 2
+
 	totalScore := 0
 	for _, line := range lines {
 
@@ -44,18 +52,25 @@ func part1(lines []string) {
 }
 
 func part2(lines []string) {
-	fmt.Println("TODO")
+	moveMap := make(map[string]int)
+	moveMap["A X"] = 3
+	moveMap["A Y"] = 4
+	moveMap["A Z"] = 8
+	moveMap["B X"] = 1
+	moveMap["B Y"] = 5
+	moveMap["B Z"] = 9
+	moveMap["C X"] = 2
+	moveMap["C Y"] = 6
+	moveMap["C Z"] = 7
+
+	totalScore := 0
+	for _, line := range lines {
+		totalScore += moveMap[line]
+	}
+	fmt.Println(totalScore)
 }
 
-var moveMap = make(map[byte]int)
-
 func main() {
-	moveMap['A'] = 0
-	moveMap['B'] = 1
-	moveMap['C'] = 2
-	moveMap['X'] = 0
-	moveMap['Y'] = 1
-	moveMap['Z'] = 2
 
 	lines := getInputAsArray("input.txt")
 
