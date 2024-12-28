cf266feb5201f4f93ca7f7913308681471526b2b
Márton Szücs <2458236+martonsz@users.noreply.github.com>
2024-12-28 15:39:51 +0100

    2024: day05 part1 solved

---
+++ day05/aoc.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+import argparse
+import os
+
+ruleDict = {}
+
+def part1(filename: str) -> None:
+  global ruleDict, afterDict
+  sum = 0
+  with open(filename, "r") as file:
+    for line in file:
+      if line.find("|") != -1:
+        before, after = list(map(str, line.strip().split("|")))
+        ruleDict[f"{before}|{after}"] = True
+      elif not line.strip():
+        continue
+      else:
+        to_sort = [x for x in line.strip().split(",")]
+        sorted = quick_sort(to_sort)
+        if to_sort == sorted:
+          sum += int(sorted[len(sorted)//2])
+    print(sum)
+
+def quick_sort(lst: list) -> None:
+  if len(lst) <= 1:
+    return lst
+
+  pivot = lst[len(lst)//2]
+  sml = []
+  mid = []
+  big = []
+  for x in lst:
+    if f"{x}|{pivot}" in ruleDict:
+      sml.append(x)
+    elif f"{pivot}|{x}" in ruleDict:
+      big.append(x)
+    else:
+      mid.append(x)
+  return quick_sort(sml) + mid + quick_sort(big)
+
+
+def part2(filename: str) -> None:
+  pass
+
+
+def main():
+  env_part = os.getenv("part")
+  parser = argparse.ArgumentParser(description="Process input file and part flag.")
+  parser.add_argument(
+    "-i",
+    "--input",
+    type=str,
+    default="input.txt",
+    help="Input file (default: input.txt)",
+  )
+  group = parser.add_mutually_exclusive_group()
+  group.add_argument(
+    "-1",
+    action="store_const",
+    dest="part",
+    const="part1",
+    help="Set part to 'part1'",
+  )
+  group.add_argument(
+    "-2",
+    action="store_const",
+    dest="part",
+    const="part2",
+    help="Set part to 'part2'",
+  )
+  parser.add_argument(
+    "-b",
+    "--begining",
+    action="store_true",
+    help="Pring row in the begining. On windows vscode prints metadata in the beginning without a new line",
+  )
+  args = parser.parse_args()
+
+  part = args.part if args.part else env_part if env_part else "part1"
+
+  if args.begining:
+    print("")
+
+  if part == "part1":
+    part1(args.input)
+  else:
+    part2(args.input)
+
+
+if __name__ == "__main__":
+  main()
